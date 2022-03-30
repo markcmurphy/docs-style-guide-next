e643be57999675976d2feb1809d22ca94491a603
Mark Murphy <mark.murphy@bigcommerce.com>
2022-03-30 12:22:44 -0500

    textarea

--- component/SignupForm.jsx
+++ component/SignupForm.jsx
@@ -9,7 +9,7 @@ const MyTextInput = ({ label, ...props }) => {
   return (
     <>
       <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
+      <textarea className="text-input" {...field} {...props} />
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
       ) : null}
